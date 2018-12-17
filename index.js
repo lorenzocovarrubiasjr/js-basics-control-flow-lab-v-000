a19dd6d5e364d4902f73cf9697c6a33103323939
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-12-17 20:08:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet (number) {
+  if (number < 400) {
+    return "This one is on me!"
+  } 
+  else if (number > 2500) {
+    return "No can do."
+  }
+  else {
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity (city) {
+  let yes =  "Ok, sounds good.";
+  let not = 'No go.';
+  let inCityLimits = (city === "NYC") ? yes : no ;
+}
+
+function switchOnCharmFromTip (tip) {
+  switch(tip) {
+  case "generous":
+    return "Thank you so much." 
+    break;
+  case "not as generous":
+    return 'Thank you.'
+    break;
+    default:
+    return "bye"
+  }
+}
