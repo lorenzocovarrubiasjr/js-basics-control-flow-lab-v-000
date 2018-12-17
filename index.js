fda287641311de177b348175b0724f9963ef79d4
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2018-12-17 20:13:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your code in this file!
+
+function scuberGreetingForFeet (number) {
+  if (number < 400) {
+    return "This one is on me!"
+  } 
+  else if (number > 2500) {
+    return "No can do."
+  }
+  else {
+    return "I will gladly take your thirty bucks."
+  }
+}
+
+function ternaryCheckCity (city) {
+ return (city === "NYC") ? "Ok, sounds good." : 'No go.' ;
+}
+
+function switchOnCharmFromTip (tip) {
+  switch(tip) {
+  case "generous":
+    return "Thank you so much." 
+    break;
+  case "not as generous":
+    return 'Thank you.'
+    break;
+    default:
+    return "Bye."
+  }
+}
